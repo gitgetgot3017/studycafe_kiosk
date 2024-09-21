26acc8d1d64584c3ccfc3013f79ebd749c87fbf6
hyeonji lee <hyunji3017@naver.com>
2024-09-22 00:43:03 +0900

    프로젝트 시작

---
+++ src/main/java/lhj/studycafe_kiosk/StudycafeKioskApplication.java
@@ -0,0 +1,13 @@
+package lhj.studycafe_kiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudycafeKioskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudycafeKioskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lhj/studycafe_kiosk/StudycafeKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package lhj.studycafe_kiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudycafeKioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
