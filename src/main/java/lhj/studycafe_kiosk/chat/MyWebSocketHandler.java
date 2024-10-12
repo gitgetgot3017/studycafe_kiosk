0906e81fdb7cebf472932cc32a23b8b91277613c
hyeonji lee <hyunji3017@naver.com>
2024-10-12 23:29:11 +0900

    feat: 스프링부트 기반 웹소켓 서버 구축

---
+++ src/main/java/lhj/studycafe_kiosk/chat/MyWebSocketHandler.java
@@ -0,0 +1,28 @@
+package lhj.studycafe_kiosk.chat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+@RequiredArgsConstructor
+public class MyWebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+
+    }
+}
