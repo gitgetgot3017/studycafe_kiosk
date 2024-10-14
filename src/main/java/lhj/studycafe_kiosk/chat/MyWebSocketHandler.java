b3b4a398ef282d9ea24230c2f0b5b05434d93122
hyeonji lee <hyunji3017@naver.com>
2024-10-15 00:22:33 +0900

    docs: 채팅 구현 방법 요약

--- src/main/java/lhj/studycafe_kiosk/chat/MyWebSocketHandler.java
+++ src/main/java/lhj/studycafe_kiosk/chat/MyWebSocketHandler.java
@@ -14,10 +14,20 @@ public class MyWebSocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 
+        // 채팅방에 처음 입장하는 경우, 입장 메시지를 DB에 저장
+
+
+        // DB에서 채팅 메시지 가져오기
+
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+
+        // 서버로 전달된 채팅 메시지를 DB에 저장하기
+
+
+        // 채팅방에 존재하는 모든 멤버에게 채팅 메시지 전송
 
     }
 
