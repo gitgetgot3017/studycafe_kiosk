ff96b78a5e0ea819a71e2984143e34e7adee647c
hyeonji lee <hyunji3017@naver.com>
2024-12-02 19:26:27 +0900

    fix: 메인 입실 페이지에서 유저의 이용 정보 출력하는 기능 커밋 파일 추가

---
+++ src/main/java/lhj/studycafe_kiosk/seat/dto/MySeatResponse.java
@@ -0,0 +1,12 @@
+package lhj.studycafe_kiosk.seat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MySeatResponse {
+
+    private Long id;
+    private String entranceCode;
+}
