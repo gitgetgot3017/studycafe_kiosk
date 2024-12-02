f331b871cbd04c1ab8baa696618be14facdd2720
hyeonji lee <hyunji3017@naver.com>
2024-12-02 19:16:50 +0900

    feat: 메인 입실 페이지에서 이용권 조회 및 변경 기능

--- front/src/pages/main/MainIn.js
+++ front/src/pages/main/MainIn.js
@@ -5,6 +5,7 @@ import {changeUserInOut} from "../../store";
 
 function MainIn() {
 
+    let [subscriptions, setSubscriptions] = useState([]);
     let [mySeatNum, setMySeatNum] = useState(0);
     let [myEntranceCode, setMyEntranceCode] = useState("");
     let [itemName, setItemName] = useState("");
@@ -40,53 +41,6 @@ function MainIn() {
             });
     }, []);
 
-    // let [subscriptions, setSubscriptions] = useState([]);
-    // let [beforeSubscriptionId, setBeforeSubscriptionId] = useState();
-
-    // function showSubscriptions() {
-    //
-    //     axios.get("/subscriptions")
-    //         .then((result) => {{
-    //             setSubscriptions(result.data);
-    //         }})
-    //         .catch((error) => {
-    //             console.error("이용권 조회 중 에러 발생:", error.response ? error.response.data : error.message);
-    //             if (error.response) {
-    //                 console.error("에러 상태 코드:", error.response.status);
-    //             }
-    //         });
-    // }
-
-    // useEffect(() => {
-    //     let representativeSubscription = subscriptions.find(subscription => subscription.representative);
-    //     if (representativeSubscription) {
-    //         setBeforeSubscriptionId(representativeSubscription.subscriptionId); // 조건을 만족하는 subscription의 id 저장
-    //     }
-    // }, [subscriptions]);
-
-    // function changeSubscription(afterSubscriptionId) {
-    //
-    //     let result = window.confirm("이용권을 변경하시겠습니까?");
-    //
-    //     if (!result) {
-    //         return;
-    //     }
-    //
-    //     axios.patch("/subscriptions", {
-    //         beforeSubscriptionId: beforeSubscriptionId,
-    //         afterSubscriptionId: afterSubscriptionId
-    //     })
-    //         .then(() => {
-    //             alert("이용권이 변경되었습니다!");
-    //         })
-    //         .catch((error) => {
-    //             console.error("이용권 변경 중 에러 발생:", error.response ? error.response.data : error.message);
-    //             if (error.response) {
-    //                 console.error("에러 상태 코드:", error.response.status);
-    //             }
-    //         });
-    // }
-
     return (
         <div>
             {/* Main Info Section */}
@@ -94,7 +48,21 @@ function MainIn() {
                 <div className="card shadow rounded-4 text-center p-4">
                     <div className="d-flex justify-content-between mb-3">
                         <span className="text-muted">이용 정보</span>
-                        <p className="text-decoration-none" /* onClick={showSubscriptions} */ data-bs-toggle="modal" data-bs-target="#subscriptionListModal" style={{cursor: "pointer"}}>이용권 변경</p>
+                        <p className="text-decoration-none" data-bs-toggle="modal" data-bs-target="#subscriptionListModal"
+                           onClick={() => {
+                               axios.get("/subscriptions")
+                                   .then((result) => {
+                                       {
+                                           setSubscriptions(result.data);
+                                       }
+                                   })
+                                   .catch((error) => {
+                                       console.error("이용권 조회 중 에러 발생:", error.response ? error.response.data : error.message);
+                                       if (error.response) {
+                                           console.error("에러 상태 코드:", error.response.status);
+                                       }
+                                   })
+                           }}>이용권 변경</p>
                     </div>
                     <h1 className="display-3 mb-3">{mySeatNum}</h1>
                     <p className="badge bg-primary mb-1">이용중</p>
@@ -137,14 +105,33 @@ function MainIn() {
                         <div className="modal-body">
                             <p>
                                 {
-                                    // subscriptions.map(function(subscription, i) {
-                                    //     return (
-                                    //         <div style={{background: "skyblue", marginBottom: "15px", cursor: "pointer"}} /* onClick={() => changeSubscription(subscription.subscriptionId)} */ key={i}>
-                                    //             <div>{subscription.itemName}</div>
-                                    //             <div>{subscription.leftTime}</div>
-                                    //         </div>
-                                    //     );
-                                    // })
+                                    subscriptions.map(function(subscription, i) {
+                                        return (
+                                            <div style={{background: "pink", marginBottom: "15px", cursor: "pointer"}} key={i}
+                                                 onClick={() => {
+                                                     let result = window.confirm("이용권을 변경하시겠습니까?");
+                                                     if (!result) {
+                                                         return;
+                                                     }
+
+                                                     axios.patch("/subscriptions", {
+                                                         afterSubscriptionId: subscription.subscriptionId
+                                                     })
+                                                         .then(() => {
+                                                             alert("이용권이 변경이 완료되었습니다.");
+                                                         })
+                                                         .catch((error) => {
+                                                             console.error("이용권 변경 중 에러 발생:", error.response ? error.response.data : error.message);
+                                                             if (error.response) {
+                                                                 console.error("에러 상태 코드:", error.response.status);
+                                                             }
+                                                         });
+                                                 }}>
+                                                <div>이용권: {subscription.itemName}</div>
+                                                <div>남은 시간: {subscription.leftTime}</div>
+                                            </div>
+                                        );
+                                    })
                                 }
                             </p>
                         </div>
