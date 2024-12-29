273461f1f2b7b52451fe831354cd8cbc8c83f3d0
hyeonji lee <hyunji3017@naver.com>
2024-12-30 00:45:43 +0900

    feat: SSE 이벤트를 자바 메모리 -> DB 저장으로 변경

--- src/main/java/lhj/studycafe_kiosk/notification/NotificationRepository.java
+++ src/main/java/lhj/studycafe_kiosk/notification/NotificationRepository.java
@@ -1,16 +1,23 @@
 package lhj.studycafe_kiosk.notification;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lhj.studycafe_kiosk.domain.NotificationEvent;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class NotificationRepository {
 
+    @PersistenceContext
+    EntityManager em;
+
     private Map<Long, SseEmitter> sseEmitters = new ConcurrentHashMap<>(); // SseEmitter 저장소
-    private Map<String, String> events = new ConcurrentHashMap(); // 이벤트 저장소 TODO: 추후 DB로 변경 예정
 
     public void saveSseEmitter(Long memberId, SseEmitter sseEmitter) {
         sseEmitters.put(memberId, sseEmitter);
@@ -24,18 +31,25 @@ public class NotificationRepository {
         return sseEmitters.get(memberId);
     }
 
-    public Map<String, String> findAllEvents(Long memberId) {
+    public Map<String, NotificationEvent> findAllEvents(Long memberId) {
 
-        Map<String, String> result = new ConcurrentHashMap<>();
-        for (Map.Entry<String, String> entry : result.entrySet()) {
-            if (entry.getKey().startsWith(memberId + "_")) {
-                result.put(entry.getKey(), entry.getValue());
-            }
+        List<NotificationEvent> events = em.createQuery("select n from NotificationEvent n where n.pushed = false and n.id like :memberId", NotificationEvent.class)
+                .setParameter("memberId", memberId + "_%")
+                .getResultList();
+
+        Map<String, NotificationEvent> result = new ConcurrentHashMap<>();
+        for (NotificationEvent event : events) {
+            result.put(event.getId(), event);
         }
         return result;
     }
 
-    public void saveEvent(String id, String event) {
-        events.put(id, event);
+    public void saveEvent(String id, String event, boolean pushed) {
+        NotificationEvent notificationEvent = new NotificationEvent(id, event, pushed, LocalDateTime.now());
+        em.persist(notificationEvent);
+    }
+
+    public void updateEventPushed(NotificationEvent notificationEvent) {
+        notificationEvent.pushedToUser();
     }
 }
--- src/main/java/lhj/studycafe_kiosk/notification/NotificationService.java
+++ src/main/java/lhj/studycafe_kiosk/notification/NotificationService.java
@@ -1,5 +1,6 @@
 package lhj.studycafe_kiosk.notification;
 
+import lhj.studycafe_kiosk.domain.NotificationEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,10 +35,12 @@ public class NotificationService {
 
         // 4: 클라이언트와 서버 간 SSE 연결이 종료되어 클라이언트가 재요청한 경우
         if (!lastEventId.equals("")) {
-            Map<String, String> events = notificationRepository.findAllEvents(memberId);
+            Map<String, NotificationEvent> events = notificationRepository.findAllEvents(memberId);
             events.entrySet().stream()
-                    .filter(entry -> lastEventId.compareTo(entry.getKey()) < 0)
-                    .forEach(entry -> sendToClient(sseEmitter, entry.getKey(), entry.getValue(), memberId));
+                .forEach(entry -> {
+                    sendToClient(sseEmitter, entry.getKey(), entry.getValue().getMessage(), memberId);
+                    notificationRepository.updateEventPushed(entry.getValue());
+                });
         }
 
         return sseEmitter;
@@ -49,11 +52,15 @@ public class NotificationService {
         String id = memberId + "_" + System.currentTimeMillis();
         String event = "예약하신 좌석 이용 가능합니다.";
 
-        notificationRepository.saveEvent(id, event);
-        sendToClient(sseEmitter, id, event, memberId);
+        try {
+            sendToClient(sseEmitter, id, event, memberId);
+            notificationRepository.saveEvent(id, event, true);
+        } catch (RuntimeException e) {
+            notificationRepository.saveEvent(id, event, false);
+        }
     }
 
-    private void sendToClient(SseEmitter sseEmitter, String id, Object data, Long memberId) {
+    private void sendToClient(SseEmitter sseEmitter, String id, String data, Long memberId) {
 
         try {
             sseEmitter.send(SseEmitter.event()
@@ -61,6 +68,7 @@ public class NotificationService {
                     .data(data));
         } catch (IOException e) {
             notificationRepository.deleteSseEmitter(memberId);
+            throw new RuntimeException("SSE 데이터 전송 실패!");
         }
     }
 }
