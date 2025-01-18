92b98aab563ded116772b8b0dc2777b8de570b37
hyeonji lee <hyunji3017@naver.com>
2025-01-18 23:29:05 +0900

    remove: 회원 당 이용권을 1개만 가질 수 있도록 제한

--- src/main/java/lhj/studycafe_kiosk/subscription/exception/SubscriptionChangeException.java
+++
@@ -1,8 +0,0 @@
-package lhj.studycafe_kiosk.subscription.exception;
-
-public class SubscriptionChangeException extends RuntimeException {
-
-    public SubscriptionChangeException(String message) {
-        super(message);
-    }
-}
