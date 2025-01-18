f6f58df1b5c3206cb52df4fc1d5bb4796be726b1
hyeonji lee <hyunji3017@naver.com>
2025-01-18 18:37:48 +0900

    fix: 배포 시 필드 실패 해결 위함

--- src/test/java/lhj/studycafe_kiosk/StudycafeKioskApplicationTests.java
+++ src/test/java/lhj/studycafe_kiosk/StudycafeKioskApplicationTests.java
@@ -1,14 +1,14 @@
 package lhj.studycafe_kiosk;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class StudycafeKioskApplicationTests {
 
 	@Test
-	@Disabled("테스트 환경 설정 중. 추후 활성화 필요")
 	void contextLoads() {
 	}
 
